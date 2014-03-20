81ad29713650535d7b7a32fb41cf24315533810d
joelolofsson <joel.olofsson@gmail.com>
2014-03-20 15:29:30 +0100

    cleaning up repo

--- grlib-gpl-1.3.4-b4140/designs/leon3-altera-ep3c25-eek/lcd.in.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONFIG_LCD_ENABLE
-#define CONFIG_LCD_ENABLE 0
-#endif
-#ifndef CONFIG_LCD3T_ENABLE
-#define CONFIG_LCD3T_ENABLE 0
-#endif
-
--- grlib-gpl-1.3.4-b4140/designs/leon3-digilent-atlys/systest.c
+++
@@ -1,8 +0,0 @@
-
-int main(void)
-{
-    report_start();
-    base_test();
-    greth_test(0x80000e00);
-    report_end();
-}
