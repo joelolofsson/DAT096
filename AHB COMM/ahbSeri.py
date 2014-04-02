b3de84f05daf07c4814d188c8e7e5090f66bcadc
Syannako <syannako@gmail.com>
2014-04-02 19:58:26 +0200

    The AHB code now works flawlesly (to my knowledge). It has been tested with various bit patterns and for various memories. It remains to be connected to the GUI code with an intermediate code to map the Data into hex numbers.

--- AHB COMM/ahbSeri.py
+++ AHB COMM/ahbSeri.py
@@ -2,51 +2,57 @@ import SeriL
 from struct import *
 
 def ahbread(addrr):
-	leng='0100'
-	id=1000
-	
+	leng='0000'
+	id='1000'
+	idlen=id+leng
+	print "idlen: " , idlen
 	#HEX DATA
 	chr_addrr = [int(addrr[2:4], 16),int(addrr[4:6], 16), int(addrr[6:8], 16),int(addrr[8:10], 16),]
 	
 	#CHARACTERS
 	converted_addr=[chr(chr_addrr[0]),chr(chr_addrr[1]),chr(chr_addrr[2]),chr(chr_addrr[3]),]
 	
-	idleng=int(str(id)+leng, base=2)
-		
-	print chr_addrr, converted_addr
-	x= chr(idleng)+(''.join(str(z) for z in converted_addr)) 
+	idleng=int((idlen), base=2)
+	print "idleng: " , idleng, chr(idleng)
+	#print chr_addrr, converted_addr
+	#x= chr(idleng)+(''.join(str(z) for z in converted_addr)) 
 	
-	print "\n The converted message is:" 
-	print	x 
+	#print "\n The converted message is:" 
+	#print	x 
 	packed = pack('Bcccc', idleng,converted_addr[0],converted_addr[1],converted_addr[2],converted_addr[3])
-	print packed
+	#print packed
 	indata=SeriL.SeriLeon(packed)
-	
-	print "Indata: "
-	print  indata, "\n"
-	print "Length:", len(indata),  "\n"
-	#indata= raw_input("Please enter something: ") #REMEMBER TO ADD A COMMUNICATION HERE. the data should be read by the leon
-	
+	print "the length is: ",  len(indata)
+	unpacked=unpack('I', indata)
+	#print "Indata: ", indata, "\n"
+	data=hex(unpacked[0])
+	Reverse_data=data[0:2]+data[8:10]+data[6:8]+data[4:6]+data[2:4]
+	print "The Data are: " + data
+	print "The Reversed Data are: " + Reverse_data
+
+
 	
 def ahbwrite(addrr,data):
-	leng='000000'
-	id=11
+	leng='0000'
+	id='1100'
 	
 	chr_addrr = [int(addrr[2:4], 16),int(addrr[4:6], 16), int(addrr[6:8], 16),int(addrr[8:10], 16),]
+	
 	converted_addr=[chr(chr_addrr[0]),chr(chr_addrr[1]),chr(chr_addrr[2]),chr(chr_addrr[3]),]
 	
 	chr_data = [int(data[2:4], 16),int(data[4:6], 16), int(data[6:8], 16),int(data[8:10], 16),]
 	converted_data=[chr(chr_data[0]),chr(chr_data[1]),chr(chr_data[2]),chr(chr_data[3]),]
 	
-	idleng=int(str(id)+leng, base=2)
+	idleng=int(id+leng, base=2)
 	#PackedMessage=[chr(idleng),converted_addr,converted_data]
 	
 	x=chr(idleng)+(''.join(str(z) for z in converted_addr))+(''.join(str(y) for y in converted_data))   #''.join(PackedMessage)
 	
-	print "\n The converted message is:" 
-	print x 
-	indata=SeriL.SeriLeon(x)
-	print indata
+	packed = pack('Bcccccccc', idleng,converted_addr[0],converted_addr[1],converted_addr[2],converted_addr[3],
+	converted_data[0],converted_data[1],converted_data[2],converted_data[3])
+	print "\n The converted message is:" , packed
+	indata=SeriL.SeriLeon(packed)
+	
 	
 	
 inusr=raw_input("Please select mode, R for read, W for write: ")
