599fd5b1d1e0a138bda9f250803bd1f2f660c0e3
joelolofsson <joel.olofsson@gmail.com>
2014-04-08 23:15:05 +0200

    Made an embarrising thing, wont mention it

--- Daniel GRLIB/grlib-gpl-1.3.4-b4140/designs/leon3-digilent-nexys4/vivado/leon3-digilent-nexys4/leon3-digilent-nexys4.srcs/sources_1/imports/rtl/DAC_TOP.vhd
+++ Daniel GRLIB/grlib-gpl-1.3.4-b4140/designs/leon3-digilent-nexys4/vivado/leon3-digilent-nexys4/leon3-digilent-nexys4.srcs/sources_1/imports/rtl/DAC_TOP.vhd
@@ -1,85 +1,109 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 21.03.2014 16:23:07
--- Design Name: 
--- Module Name: DAC_buffer - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
+entity dacTop is
+	port(
+		rstn	:	in STD_LOGIC;	 	
+		clk	:	in STD_LOGIC;
+		data	:	in STD_LOGIC_VECTOR(15 downto 0);
+		addr	:	in STD_LOGIC_VECTOR(6 downto 0);
+		write	:	in STD_LOGIC;
+		sampleclk : out STD_LOGIC;
+		sampleclk44khz : out STD_LOGIC;
+		sclk	:	out STD_LOGIC;
+		din	:	out std_logic;
+	 	nSync	:	out STD_LOGIC
+	);
+end entity dacTop;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+architecture behavioral of dacTop is 
 
-entity DAC_buffer is
-    generic (
-        bufferwidth: integer:=7);
-  Port ( 
-        clk		:	in STD_LOGIC;
-        rst		:	in STD_LOGIC;
-        buffRead	:	in STD_LOGIC;
-        indexReset	:	in STD_LOGIC;
-	      buffWrite	:	in STD_LOGIC;
-       	buffIn		:	in STD_LOGIC_VECTOR (15 downto 0);
-        buffOut		:	out STD_LOGIC_VECTOR (15 downto 0);
-        addr		:	in STD_LOGIC_VECTOR(bufferwidth-1 downto 0));
-end DAC_buffer;
-
-architecture Behavioral of DAC_buffer is
+component clk_divide
+	generic(
+				systemclock : integer:=100000000;
+				sampleclock : integer:=44100;
+				OSR : integer:=16);
+	port(
+		rst	:	in STD_LOGIC;	 	
+		clk	:	in STD_LOGIC;
+		clk50MHz : out STD_LOGIC;
+		clk25MHz : out STD_LOGIC;
+		clk705kHz : out STD_LOGIC;
+		clk44kHz		: out STD_LOGIC
+	);
+end component;
 
 
-type Memory_array_type is array (0 to 2**bufferwidth-1) of STD_LOGIC_VECTOR(15 downto 0);
 
-signal Memory_array : Memory_array_type;
+	component DAC_SPI
+		port(
+			rstn	:	in STD_LOGIC;	 	
+			clk	:	in STD_LOGIC;
+			data	:	in STD_LOGIC_VECTOR(15 downto 0);
+			sampleclk : in STD_LOGIC;
+			din	:	out std_logic;
+		 	nSync	:	out STD_LOGIC;
+			ready	:	out std_logic  
+		);
+	end component DAC_SPI;
+	
+	component DAC_buffer is
+		generic (
+			bufferwidth: integer:=7);
+		Port ( 
+			clk		:	in STD_LOGIC;
+			rst		:	in STD_LOGIC;
+			buffRead	:	in STD_LOGIC;
+			indexReset	:	in STD_LOGIC;
+			buffWrite	:	in STD_LOGIC;
+			buffIn		:	in STD_LOGIC_VECTOR (15 downto 0);
+			buffOut		:	out STD_LOGIC_VECTOR (15 downto 0);
+			addr		:	in STD_LOGIC_VECTOR(bufferwidth-1 downto 0));
+	end component DAC_buffer;
+	
+	signal sBuffOut : std_logic_vector(15 downto 0);
+	signal readBuffer : std_logic;
+	signal clk50MHz : STD_LOGIC;
+	signal clk25MHz : STD_LOGIC;
 
-signal lastread : STD_LOGIC;
-
-signal read_index : integer range 0 to 2**bufferwidth-1;
 begin
-process(clk,rst)
-begin
-    if rst = '0' then
-        buffout <= (others => '0');
-        read_index <= 0;
-        Memory_array <= (others => (others => '0'));
-    elsif rising_edge(clk) then
-				lastread <= buffread;
-        if (buffRead = '1') and (lastread = '0') then
-            buffout <= Memory_array(read_index);
 
-            if read_index = 2**bufferwidth -1 then
-                read_index <= 0;
-            else
-                read_index <= read_index + 1 ;
-            end if;
-        end if;
-        if indexReset = '1' then
-           read_index <= 0;
-        end if;
-		if buffWrite = '1' then
-			Memory_array(to_integer(unsigned(addr))) <= buffin;     
-		end if;
-    end if;
-end process;
+sampleclk44kHz <= readbuffer;
+sclk <= clk25MHz;
 
-end Behavioral;
+	inst_clk_divider : clk_divide
+	port map(
+		rst => rstn,
+		clk	=> clk,
+		clk50MHz	=> clk50MHz,
+		clk25MHz 	=> clk25MHz,
+		clk44kHz => readbuffer,
+		clk705kHz => sampleclk);
+
+
+	inst_DAC_SPI : DAC_SPi
+	port map(
+		rstn		=> rstn,
+		clk		=> clk25MHz,
+		data		=> sBuffOut,
+		sampleclk => readbuffer,
+		din		=> din,
+		nSync		=> nSync,
+		ready		=> open
+		);
+	
+	inst_DAC_BUFFER : DAC_BUFFER
+		port map(
+		clk		=> clk,
+		rst		=> rstn,
+		buffRead	=> readBuffer,
+		indexReset	=> '0',
+		buffWrite	=> write,
+		buffIn		=> data,
+		buffOut 	=> sBuffOut,
+		addr		=> addr
+		);  
+		    
+		
+		
+end architecture behavioral;
