962050bb444ad5372ce841285f7c98b91d9bacb1
Jaxc <coolJaxc@gmail.com>
2014-04-16 09:30:46 +0200

    the actual nothing update

---
+++ grlib-gpl-1.3.4-b4140/designs/leon3-digilent-nexys4/vivado/leon3-digilent-nexys4/leon3-digilent-nexys4.srcs/sources_1/imports/rtl/PWM.vhd
@@ -0,0 +1,83 @@
+-- ! @file
+-- ! @brief PWM module for DAC
+
+
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    09:28:50 02/13/2014 
+-- Design Name: 
+-- Module Name:    PWM - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+
+ 
+-- ! Use of standard library
+library IEEE;
+-- ! Use of standard logic arguments
+use IEEE.STD_LOGIC_1164.ALL;
+-- ! Use of standard numeric arguments
+use IEEE.Numeric_std.all;
+
+-- !  This component handles the conversion from digital to analogue signals
+entity PWM is
+	 Generic(
+			  Resolution : INTEGER :=11);							-- ! A generic to decide the resolution
+    Port ( PWMclk : in  STD_LOGIC;									-- ! A clock running at 44100 * 2^resolution
+			  rst  : in STD_LOGIC;								-- ! Global reset active low
+			  value  : in STD_LOGIC_VECTOR(Resolution -1 downto 0);	-- ! Value to be outputted
+			  sampleEna705KHz : out STD_LOGIC;					-- ! Generated 705.6 KHz enable for the ADC	
+			  sampleEna44KHz : out STD_LOGIC;
+           PWMout : out  STD_LOGIC);								-- ! PWM out of the value
+end PWM;
+-- ! @brief Architecture of the PWM-
+-- ! @details The architecture containing the main body of the component.
+architecture Behavioral of PWM is
+
+signal cnt,lastcnt : integer range 0 to 2**Resolution-1;			-- ! Counters for the PWM
+signal samplevalue : std_logic_vector(Resolution-1 downto 0);		-- ! Sampled input value to make sure it doesn't change during the transmission
+
+
+begin
+
+process(PWMclk,rst)												-- ! Process for creating the PWMout and sampleEna705KHz
+begin
+	if rst = '0' then												-- ! Asynchronous reset
+		cnt <= 2**11-1;												-- ! cnt set to maximum value to make it read a new sample as first thing after the reset
+		PWMout <= '0';
+		sampleEna705Khz <= '0';
+	elsif rising_edge(pwmclk) then									-- ! The synchronous part of the process is triggered by the pwm clk
+		sampleEna44KHz <= '0';
+		if cnt = 2**Resolution-1 then								-- ! When the counter reaches the maximum value:
+			PWMout <= '1';											-- ! PWMout is set to '1' until cnt reaches the samplevalue
+			cnt <= 0;												-- ! cnt is reseted
+			samplevalue <= value;									-- ! A new samplevalue is given
+		    SampleEna44KHz <= '1';
+		elsif cnt = to_integer(unsigned(samplevalue)) then			-- ! When the counter reaches the value of the samplevalue:
+			PWMout <= '0';											-- ! PWMout is set to one
+			cnt <= cnt +1;											-- ! cnt is increased
+		else														-- ! otherwise:
+			cnt <= cnt +1;											-- ! cnt is increased
+		end if;
+        lastcnt <= cnt;												-- ! lastcnt is designated the value of current cnt
+
+		if ((cnt MOD 128) = 0) then -- and (lastcnt MOD 128) = 127) or  then		-- ! When the current cnt is a multiple of 128 and when last cnt:
+			sampleEna705Khz <= '1';									-- ! The sampleEna705KHz is set to '1' for one clockcycle
+		else														-- ! Otherwise:
+	        sampleEna705Khz <= '0';									-- ! it is set to '0'
+	end if;
+end if;
+end process;
+end Behavioral;
+
---
+++ grlib-gpl-1.3.4-b4140/designs/leon3-digilent-nexys4/vivado/leon3-digilent-nexys4/leon3-digilent-nexys4.srcs/sources_1/ip/ila_0_0/labtools_general_components_lib_v2_0/hdl/verilog/cs_cfglut6.v
@@ -0,0 +1,82 @@
+`pragma protect begin_protected
+`pragma protect version = 1
+`pragma protect encrypt_agent = "XILINX"
+`pragma protect encrypt_agent_info = "Xilinx Encryption Tool 2013"
+`pragma protect key_keyowner = "Cadence Design Systems.", key_keyname= "cds_rsa_key", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 64)
+`pragma protect key_block
+fkztzhEUT91i0l27DBAeJ5c3kRgWXH3TqiNb6LvTLzrmlMachRBjvA+ppEww65UtpRmM5z6E9Fwm
+jyZfcrMbGQ==
+
+`pragma protect key_keyowner = "Mentor Graphics Corporation", key_keyname= "MGC-VERIF-SIM-RSA-1", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 128)
+`pragma protect key_block
+SFj2N9PbpDE5itilTbAa2t+Cq6vc/iVn6NE8FeGwn7edWYLX9buh0fVk6TjdBmVUMbq4Nrdl5GuJ
+3/GpAZNw2HnSDpWjQB4q6a3Yy2NNLcPj3uriYXEW6UF9Wox7NgmpAUw/xI7F6AP/Ugi81SwW6Ijt
+IeowEz4ly2IqVbBB/h4=
+
+`pragma protect key_keyowner = "Xilinx", key_keyname= "xilinx_2013_09", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 256)
+`pragma protect key_block
+4x5EDN2Kbpc5PXWNiXDmN4MAmfqC9UHuYu7YtB/K1K6RmJd/bkJ/XUEvQKhiBtkcR3cUAyQS9DUk
+ARpdYkGY34wsQ5J37Mq9I+C3cxdkCPJANjnLEIh4w6YnfqnNbbt5l1yy0+ulVqtN1SoVpJqoFUon
+d5e5bkDESEDJpEF+zREKkuJcaqqsXn4r2zwcnpp3qYNrCwUa+U2cXeL8wMg3C3Oy6EgiTYnP3yzE
+6Ota2YR89Tpcy1m3mghgXogtvMmkpi8l3bOBA46ImAVkJnbNFELEA+NIf9f6rIc8Yrn4w+vzVC0T
+yy+i9NdjbySF7CQIg0vQcL686r78wB/y80OXag==
+
+`pragma protect key_keyowner = "Synopsys", key_keyname= "SNPS-VCS-RSA-1", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 128)
+`pragma protect key_block
+bPrbrZNqxk/ryUZ8mb6HAW3v1x6jdsW83gpcq0Pp3lTXcaZgPo5oofBIYHnISGKS5XsHqrVscwc1
+BfvBcbrvFBFDfcue5cea+7tqNuvQJ7gRi1NkVyoPd9oVDLaq6Q7aBLFv3+2Vf702o+ix5L42DWwC
+0cjaB4j/kLMjH1bLW+A=
+
+`pragma protect key_keyowner = "Aldec", key_keyname= "ALDEC08_001", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 256)
+`pragma protect key_block
+fvpnr4RnwozmVNA7cKPkGzAK6+Fb3azyNv1C+zp1hpnGYfe4tizJx1EgqB+v0ZjqPKF5w1dXg8xd
+jl3XqXl0y0b4NHFFDrsTsX/fNkVpXvqS667LNPhKseYe5nJe6EiYP2NzHbxbK9ruDFqMRQMOdkCb
+hw9dJ5mWhtU4JJAfBxY4g7VNq4ruW1Viz0l47c/j4TT/lQ/h70BSPPOGMm5Ed+kpng/muWoH5CAE
+h/4tYgKC3Z9icETOpXcL8hjOll+AxWM0+LkGmTq5elekhruQ1jAuLb3DWd7XRdaHw1VisluXen/c
+Ejxz9HgRAMktJxcr1sHLYNM5pc9aXztwjxMyNA==
+
+`pragma protect data_method = "AES128-CBC"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 2032)
+`pragma protect data_block
+9f7gmNFGjnLSyxq3CKz23d4qG1bLEmR3EqrEDSRTMxJx3OaJeRpBlzqJlMacVDrkTxjRi4/+MA6G
+EpQeSLx+I6xoz2oGm8ffMBVgwRDpp4A8qdhXFXMIw/DgUFFERW/8QesSapNA6bQcJnnpvFLuUsJd
+TuByJWrkSaLiRKFutsAb1CRIYb+Kko9d2LykNE74b14BFjR2KtAP4c45Kry2JJFjvO31aUwW9boL
+dy9L+CxsDRTMXHDb7H+59a8E+XctnIYcUmywnYR9kz79FKubGGtXtxcgcQ3XFc+mnOVl4uR0FRLg
+Mc20H0nMQVu9QnMUBOz2plyl5E1TFV2OUJb5ip60zAeaHOzlQIbzKSeGmVxLygtBCc78+9fxsSeR
+0yGqr7f1KmQGzZU/g1HFYPsrIATCJsRH0SgsSRfLh2F9yc27OOcGqjuDjjS40EqAk7GNuBR+zbjV
+w9m1T1fP2rZdgF/HbiXrJl3vSlYCgciRk88CSem2zTjsc/CC6Du6C7v7JOtIbhJDtsSsvi7RWqf0
+S0b/XCT/GlkmgLWqJMucVSzMsZL8fVr2kj3kTNXEfwNg50uSy74IyQwlnGkRvUWVTWjbFpcv2zKR
+JbK+E9o+YCuPCXaQNcw1Ros2B+Kg1l3lNF/2E/9r7T0WaTGI5VkEGsc/ZlS9DVnDoGlhwYnDxw8K
+P8+zPckqAYspavAWZ4vSYfGxSdypIqjq46cz4wps9vVLmnKHLjUrjcbiIH+CKkS/PHVFUbPBRaGe
+UfXkt4eS+KalPWDLtBJVKmJfy0RrPHaOwf2b/z/LWntLq7lNNkkGLPlTjF8O1SGhEaO7647kODG7
+pOqRtYyJEtHkcM9QYclnRij+1LXfmhoYXIePBvallJ3+ICpQGNsT0n6/GHNijsgzqZz0MXnqJnEG
+OxVrb8Lkjf7mY1kir654jd3Tp2M6Pw3HcrMoPZ4zU4I78Vbwyn2LWZgo8QW670jbYgT7rMIGl6DA
+9qpBVVuVZlnFlKDh4RmFEWqfmENEol72Sv4k2GLQWOXtTqKy9rbzsUC49d3FlBe7pc+IeTCJbwmo
+Yp0+mxZZGLcn3/6VVmuE/nHjjDSTqBoY4l0n9ioVx0qkvOoPqfvkSUBCawfWqCMJCfGIyPltwX1Z
+XWzunFz4KYc30ATKFDz+6V0iyb7XkUPWFxTJKOiX6R4aVdaxhojM1ElceBaFnlcPlNiWkqbz3v54
+fkBFCrX7VYaGCpkPSeL9cIT83v2z/kIZKZ/6Ui5/VyI/TYbTT+hc34gCZwOw7s5kn46os++Qommp
+7Q/mNPyBOLorU8FssHZAY8JgalZus9a4MFsh4x/3yztEpUBxk9hC7ZajklHeubhe9XL4xoBNBdVu
+dSd+mQydK2a+947m7nosB31tj3nW6KECYM4/FAsTvxhaklG9uXBjHKb3VcLPUPbMgttB9i4LP2E4
+YRFhptdrGcVVndUbsE58sAmfzxxfAwbR7ZDksAYdBm4dlIJoQ/hRDe6ZVBfVgAAOwyfW78JAdBU+
+apJloWoRxoQfKF70jTLV2HK+Lg/pFW5t7NmUkbvMeAlJPFP7FBtbpW5TfEpu4VlLPBmVqewVVfk3
+CGa6x2DObdcDowcQVnmbEe30B/uoncJ/RL5r8sX6kUzRpg2ifdOGlNtNrurr31DYFyWzYnZY2S/R
+mz2Apr0mJGX2aZ8TGLDU4lH2mzsAfzAMZ4bWPMaWGfSiggijH/dfk6z7VLg9Kxs7glv41dqCMcnw
+3nCdVpqqATWnVPLDkWcKpRe/9MEzwoKP3c8o5Q5tPb7XA9qUsC8Q/7ayVsQrheOpJDLXX4h5bqgB
+M/CRTyTSqD/5hE+5Cfy4xMsJDzKSRbhwH85F+kyHNPbjN1Y/DjF3nS1nz+mRIMsq6vv9b/k0cEJ3
+iDU7WMdt55xwbbMeQhZ2xdU3W4yOohf2cVv0U7ydeg6LMtk5ztapmkIy4md3TpodJ1htSqpBGRhV
+hkug9zzFG7teppn4hx+qpdG3UIh+eAcIGnR5xMIAOiBNhzbGp/LDbkEFOLAd4ahvCWwXcZ9fe5e8
+p6v6jcBvKN1Eww93KfWldGnPicEfZ5lKDJXa3jaiGr8tbp0G74iBMX6e5no1uBleWlQYR/nw9urA
+bFzIZgHuZvECXoeW9VXx2XAqHVFy5KGOxf9tH/tpLpAxhxaadg19ttFRjqxM/rhhpUsixADJL6N+
+ilwPNM8SUoGHeS79BvZJGAnc4uiQMNdO+9pLA+ucqsGclmBPWV9JQTGE8esspQijvzFq/w41/bjS
+J37RyKCHKZ1cTxU7M9C1CQL8foAI0eKmXMR/++/aZ2GU/Lq0/HNhkX0Aigj1zWX0WeEgFNZch2T+
+sDistt7xwul1u5uw/KIbJn3YT10jsmIKuUSPMlKShZ1etr/BOdRPLikkRZKXBl1hTJaEmMW2PadK
+SzGAN3IZExY2LxEZ+LjjV7KQlgzP4wSeKnmbhiJMX8c89NmWeLjMMswo1USPKn/WnvBQLNeQ4ZnO
+cfkn1OeTKG9+/BnrU6o9sOwPWfhdcsO91+HFM4amEmqRvwMW82xmj/VtOL1OTMnJxBUVKU5XPfly
+gifkMlRcPn2p/YGgEd5uQU2wELdYccW8o/GUoE/nok2M+9FE4n+eYdtbxsDqPQT1CQDQpRf6+pQH
+Wa4fFbSFYOgdHxth5FnvHk+8ea4Kp6xS0Fjfhfry0JgN0gCg6w==
+`pragma protect end_protected
